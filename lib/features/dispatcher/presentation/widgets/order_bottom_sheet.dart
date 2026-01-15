8f930c4f8aabedb26cddee9f21766c04a5dea7cd
devdiagon <fredericktimo@gmail.com>
2026-01-15 10:52:23 -0500

    fead: add confirmation button in dispatcher order list

--- lib/features/dispatcher/presentation/widgets/order_bottom_sheet.dart
+++ lib/features/dispatcher/presentation/widgets/order_bottom_sheet.dart
@@ -29,7 +29,7 @@ class _OrferBottomSheetState extends ConsumerState<OrderBottomSheet> {
             ),
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.1),
+                color: Colors.black.withValues(alpha: 0.1),
                 blurRadius: 10,
                 offset: const Offset(0, -5),
               ),
@@ -71,6 +71,22 @@ class _OrferBottomSheetState extends ConsumerState<OrderBottomSheet> {
                   },
                 ),
               ),
+
+              SizedBox(height: 24),
+
+              ElevatedButton.icon(
+                onPressed: () {
+                  /*TODO*/
+                },
+                icon: Icon(Icons.check_circle_outline),
+                label: const Text('Confirmar Entrega'),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 14),
+                ),
+              ),
+
+              SizedBox(height: 24),
+
             ],
           ),
         );
@@ -78,63 +94,3 @@ class _OrferBottomSheetState extends ConsumerState<OrderBottomSheet> {
     );
   }
 }
-
-
-
-// class OrderBottomSheet extends StatelessWidget {
-//   const OrderBottomSheet({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return DraggableScrollableSheet(
-//       initialChildSize: 0.3,
-//       minChildSize: 0.15,
-//       maxChildSize: 0.7,
-//       builder: (context, scrollController) {
-//         return Container(
-//           decoration: BoxDecoration(
-//             color: Theme.of(context).scaffoldBackgroundColor,
-//             borderRadius: const BorderRadius.only(
-//               topLeft: Radius.circular(20),
-//               topRight: Radius.circular(20),
-//             ),
-//             boxShadow: [
-//               BoxShadow(
-//                 color: Colors.black.withValues(alpha: 0.1),
-//                 blurRadius: 10,
-//                 offset: const Offset(0, -5),
-//               ),
-//             ],
-//           ),
-//           child: Column(
-//             children: [
-//
-//               Container(
-//                 margin: const EdgeInsets.only(top: 12, bottom: 8),
-//                 width: 40,
-//                 height: 4,
-//                 decoration: BoxDecoration(
-//                   color: Colors.grey[300],
-//                   borderRadius: BorderRadius.circular(2),
-//                 ),
-//               ),
-//
-//               Expanded(
-//                 child: SingleChildScrollView(
-//                   controller: scrollController,
-//                   child: Padding(
-//                     padding: const EdgeInsets.all(24),
-//                     child: Text(
-//                       'LIST',
-//                       style: Theme.of(context).textTheme.headlineMedium,
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
