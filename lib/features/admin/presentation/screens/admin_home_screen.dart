fffaa42e92e8c2abf68d900f2d57b30af37dfb60
devdiagon <fredericktimo@gmail.com>
2026-01-18 16:25:21 -0500

    feat: add KPI to Admin home screen

--- lib/features/admin/presentation/screens/admin_home_screen.dart
+++ lib/features/admin/presentation/screens/admin_home_screen.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:packlead/features/admin/presentation/widgets/kpi_card.dart';
 
 class AdminHomeScreen extends StatelessWidget {
   const AdminHomeScreen({super.key});
 
+  final int pedidosDelDia = 45;
+  final int repartidoresActivos = 8;
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home Admin'));
+    return Container(
+      color: Colors.grey[50],
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+
+              // Orders from Today Card
+              KPICard(
+                title: "Pedidos del día",
+                icon: Icons.shopping_bag_rounded,
+                value: pedidosDelDia,
+              ),
+
+              // Available Dispatchers Card
+              KPICard(
+                title: "Repartidores activos",
+                icon: Icons.fire_truck,
+                value: repartidoresActivos,
+              ),
+            ],
+          ),
+
+          SizedBox(height: 30),
+
+
+        ],
+      ),
+    );
   }
 }
---
+++ lib/features/admin/presentation/widgets/kpi_card.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class KPICard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final int value;
+  final Color cardColor;
+  final String? subtitle;
+  final double cardSize;
+
+  const KPICard({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.value,
+    this.cardColor = Colors.white,
+    this.subtitle,
+    this.cardSize = 160
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: cardSize,
+      height: cardSize,
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: Theme.of(context).primaryColor.withValues(alpha: 0.3),
+          width: 1,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 2,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Title
+            Container(
+              constraints: BoxConstraints(
+                minHeight: 40,
+              ),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey[700],
+                ),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // Metric Value
+                      Text(
+                        value.toString(),
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[900],
+                          height: 1.0,
+                        ),
+                      ),
+
+                      // Icon
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).secondaryHeaderColor,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Icon(
+                          icon,
+                          color: Theme.of(context).primaryColor.withValues(alpha: 0.5),
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            if (subtitle != null)
+              Text(
+                subtitle!,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey[600],
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
