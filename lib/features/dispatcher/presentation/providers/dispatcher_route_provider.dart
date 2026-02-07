d0bc5f263cc8340759a66e660423ba53dbeb54b6
devdiagon <fredericktimo@gmail.com>
2026-02-07 12:27:26 -0500

    feat: add tracking service
    Get current location and update it in the UI and future firebase backend

--- lib/features/dispatcher/presentation/providers/dispatcher_route_provider.dart
+++ lib/features/dispatcher/presentation/providers/dispatcher_route_provider.dart
@@ -1,7 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:packlead/core/constants/srmc_hq.dart';
 import 'package:packlead/core/models/location.dart';
 import 'package:packlead/core/models/order.dart';
 import 'package:packlead/core/utils/map_utils.dart';
+import 'package:packlead/services/location/location_tracking_service.dart';
+
+/// Hardcoded origin location.
+Location _getHQOrigin() {
+  return SRMCHQ;
+}
 
 final routeProvider = FutureProvider.family<List<Location>, Order>(
       (ref, order) async {
@@ -17,13 +25,34 @@ final routeProvider = FutureProvider.family<List<Location>, Order>(
 );
 
 final dispatcherCurrentLocationProvider = StateProvider<Location?>((ref) {
-  return _getHQOrigin();
+  // Initially set to NULL until we get the actual location from the device's GPS.
+  return null;
 });
 
-/// Hardcoded origin location.
-Location _getHQOrigin() {
-  return Location(
-    lat: -0.218874,
-    lng: -78.521186,
+final locationTrackingServiceProvider = Provider.autoDispose<LocationTrackingService>((ref) {
+  // The service has implemented a lifecycle
+  // onLocationUpdate only calls every 5 seconds and if the distance difference
+  // is about 10 meters. ONLY when this conditions are met, the location is updated
+  // for UI usage and to send it to the backend for real-time tracking.
+  final service = LocationTrackingService(
+    onLocationUpdate: (location) {
+      // Update current dispatcher provider with new location
+      ref.read(dispatcherCurrentLocationProvider.notifier).state = location;
+
+      // await _sendLocationToBackend(location);
+    },
+    onError: (error) {
+      debugPrint('Error de ubicación: $error');
+    },
   );
-}
+
+  // Refresh when the provider is disposed (log out or app closed)
+  ref.onDispose(() {
+    service.dispose();
+  });
+
+  return service;
+});
+
+// TODO: Send location to backend
+// Future<void> _sendLocationToBackend(Location location) async {}
