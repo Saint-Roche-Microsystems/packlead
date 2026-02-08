e77bde69808cc80b7b861cb45eb8014ee825de60
devdiagon <fredericktimo@gmail.com>
2026-02-08 13:33:57 -0500

    feat: improve dispatcher marker on admin map
    Now it shows a circle with their name initials

--- lib/features/admin/presentation/screens/multi_point_realtime_map.dart
+++ lib/features/admin/presentation/screens/multi_point_realtime_map.dart
@@ -4,8 +4,9 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:packlead/core/models/dispatcher_location.dart';
 import 'package:packlead/core/utils/date_formatter.dart';
 import 'package:packlead/core/utils/map_utils.dart';
+import 'package:packlead/core/utils/marker_builder.dart';
 
-class MultiPointRealtimeMap extends ConsumerWidget {
+class MultiPointRealtimeMap extends ConsumerStatefulWidget {
   final List<DispatcherLocation> locations;
 
   const MultiPointRealtimeMap({
@@ -14,9 +15,39 @@ class MultiPointRealtimeMap extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<MultiPointRealtimeMap> createState() => _MultiPointRealtimeMapState();
+}
 
-    final markers = locations.map((location) {
+class _MultiPointRealtimeMapState extends ConsumerState<MultiPointRealtimeMap> {
+
+  Set<Marker> _markers = {};
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMarkers();
+  }
+
+  @override
+  void didUpdateWidget(MultiPointRealtimeMap oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.locations != widget.locations) {
+      _loadMarkers();
+    }
+  }
+
+  Future<void> _loadMarkers() async {
+    if(_markers.isEmpty) {
+      setState(() {
+        _isLoading = true;
+      });
+    }
+
+    final markerFutures = widget.locations.map((location) async {
+      final markerIcon = await MarkerBuilder.createMarkerIconInitials(location.name);
+
       return Marker(
         markerId: MarkerId(location.dispatcherId),
         position: LatLng(location.lat, location.lng),
@@ -24,22 +55,36 @@ class MultiPointRealtimeMap extends ConsumerWidget {
           title: location.name,
           snippet: 'Última actualización: ${DateFormatter.formatRelativeTime(location.updatedAt)}',
         ),
-        icon: BitmapDescriptor.defaultMarkerWithHue(
-          BitmapDescriptor.hueBlue,
-        ),
+        icon: markerIcon,
       );
-    }).toSet();
+    });
 
-    final bounds = MapUtils.calculateBounds(DispatcherLocation.toLocations(locations));
+    final markers = await Future.wait(markerFutures);
+
+    if (mounted) {
+      setState(() {
+        _markers = markers.toSet();
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoading && _markers.isEmpty) {
+      return Center(child: CircularProgressIndicator());
+    }
+
+    final bounds = MapUtils.calculateBounds(DispatcherLocation.toLocations(widget.locations));
 
     return GoogleMap(
       initialCameraPosition: CameraPosition(
-        target: LatLng(locations.first.lat, locations.first.lng),
+        target: LatLng(widget.locations.first.lat, widget.locations.first.lng),
         zoom: 12,
       ),
-      markers: markers,
+      markers: _markers,
       onMapCreated: (controller) {
-        if (locations.length > 1) {
+        if (widget.locations.length > 1) {
           controller.animateCamera(
             CameraUpdate.newLatLngBounds(bounds, 50),
           );
