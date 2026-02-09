9a6d17e61aeabf6e066a68144855405a1738cbed
devdiagon <fredericktimo@gmail.com>
2026-02-09 00:00:57 -0500

    feat: update user domain model

--- lib/mocks/users_mock_data.dart
+++ lib/mocks/users_mock_data.dart
@@ -8,29 +8,34 @@ class UsersMockData {
       email: 'admin@packlead.com',
       password: 'admin123',
       role: UserRole.admin,
+      name: 'Santiago Moreno'
     ),
 
     User(
       id: 'disp_001',
       email: 'carlos.disp@packlead.com',
       password: 'carlos123',
+      name: 'Carlos Méndez',
       role: UserRole.dispatcher,
     ),
     User(
       id: 'disp_002',
       email: 'ana.disp@packlead.com',
       password: 'ana123',
+      name: 'Ana López',
       role: UserRole.dispatcher,
     ),
     User(
       id: 'disp_003',
       email: 'pedro.disp@packlead.com',
       password: 'pedro123',
+      name: 'Pedro Martínez',
       role: UserRole.dispatcher,
     ),
     User(
       id: 'disp_004',
       email: 'maria.disp@packlead.com',
+      name: 'María García',
       password: 'maria123',
       role: UserRole.dispatcher,
     ),
@@ -38,6 +43,7 @@ class UsersMockData {
       id: 'disp_005',
       email: 'luis.disp@packlead.com',
       password: 'luis123',
+      name: 'Luis Torres',
       role: UserRole.dispatcher,
     ),
   ];
